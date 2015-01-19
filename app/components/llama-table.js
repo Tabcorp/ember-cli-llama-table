cdfb5e263ab333d698cb45371cb147f5a9ebf37d
j- <jh@live.com.au>
2015-01-20 10:46:55 +1100

    Added class, columns and data properties

--- app/components/llama-table.js
+++ app/components/llama-table.js
@@ -1,7 +1,18 @@
 import Em from 'ember';
 
 var LlamaTable = Em.Component.extend({
-	layoutName: 'llama-table'
+	layoutName: 'llama-table',
+	classNames: 'llama-table-component',
+
+	// column definitions
+	columns: null,
+
+	// table data
+	data: null,
+
+	init: function () {
+		// initialize state
+	}
 });
 
 export default LlamaTable;
