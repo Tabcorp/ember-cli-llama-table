25a3517e70c42448d2d3823457f978b0e71a56fa
j- <jh@live.com.au>
2015-01-23 10:25:08 +1100

    Removed controller. Don't need it.

--- addon/controllers/llama-table.js
+++
@@ -1,7 +0,0 @@
-import Em from 'ember';
-
-var LlamaTableController = Em.Controller.extend({
-
-});
-
-export default LlamaTableController;
--- app/components/llama-table.js
+++ app/components/llama-table.js
@@ -1,10 +1,8 @@
 import Em from 'ember';
-import Controller from 'llama-table/controllers/llama-table';
 
 var LlamaTable = Em.Component.extend({
 	layoutName: 'llama-table',
 	classNames: 'llama-table-component',
-	controller: Controller.create(),
 
 	// column definitions
 	columns: null,
