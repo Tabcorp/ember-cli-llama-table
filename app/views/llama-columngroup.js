1ad3a641a94f801a9f684152978ea1c668275000
j- <jh@live.com.au>
2015-01-23 16:03:42 +1100

    Update columngroup width when columns are inserted

--- app/views/llama-columngroup.js
+++ app/views/llama-columngroup.js
@@ -3,7 +3,24 @@ import Em from 'ember';
 var LlamaColumngroup = Em.View.extend({
 	classNames: 'llama-columngroup',
 
-	columns: null
+	columns: null,
+
+	updateTotalWidth: function () {
+		var total = 0;
+		this.get('childViews').forEach(function (view) {
+			total += view.getWidth();
+		});
+		this.$().width(total);
+	},
+
+	onChildViewUpdate: function () {
+		Em.run.later(this, this.updateTotalWidth, 0);
+	}.observes('childViews'),
+
+	didInsertElement: function () {
+		this._super();
+		Em.run.scheduleOnce('afterRender', this, this.updateTotalWidth);
+	}
 });
 
 export default LlamaColumngroup;
