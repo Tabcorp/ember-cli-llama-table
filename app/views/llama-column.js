e9aa01c71764bd91382414d4eacac909bb35425d
j- <jh@live.com.au>
2015-01-23 16:03:25 +1100

    Added column view getWidth()

--- app/views/llama-column.js
+++ app/views/llama-column.js
@@ -1,7 +1,11 @@
 import Em from 'ember';
 
 var LlamaColumn = Em.View.extend({
-	classNames: 'llama-column'
+	classNames: 'llama-column',
+
+	getWidth: function () {
+		return this.$().width();
+	}
 });
 
 export default LlamaColumn;
