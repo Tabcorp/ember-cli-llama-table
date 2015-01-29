4494b90ff2eb10b97459a2072d924289217fbd86
j- <jh@live.com.au>
2015-01-30 10:54:39 +1100

    Added sorting properties to header cell

--- app/views/llama-header-cell.js
+++ app/views/llama-header-cell.js
@@ -4,11 +4,26 @@ import LlamaCell from './llama-cell';
 var LlamaHeaderCell = LlamaCell.extend({
 	templateName: 'llama-header-cell',
 	classNames: 'llama-header-cell',
+	classNameBindings: ['sortByThis', 'sortByThisAscending', 'sortByThisDescending'],
 	attributeBindings: ['title'],
 	title: Em.computed.alias('column.label'),
 
 	// column definition
 	column: null,
+
+	sortProperties: Em.computed.alias('controller.sortedRows.sortProperties'),
+	sortAscending: Em.computed.alias('controller.sortedRows.sortAscending'),
+	sortDescending: Em.computed.not('sortAscending'),
+
+	sortByThis: function () {
+		var sortProperties = this.get('sortProperties');
+		var thisColumn = this.get('column.name');
+		var contained = sortProperties.contains(thisColumn);
+		return contained;
+	}.property('sortProperties.firstObject', 'column.name'),
+
+	sortByThisAscending: Em.computed.and('sortByThis', 'sortAscending'),
+	sortByThisDescending: Em.computed.and('sortByThis', 'sortDescending'),
 
 	mouseDown: function (e) {
 		if (e.which === 1) {
