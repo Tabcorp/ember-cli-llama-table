22247d55b9c41c8c87f087cdb8a50dd30215d0e0
j- <jh@live.com.au>
2015-02-02 10:30:49 +1100

    Added stress test route

---
+++ tests/dummy/app/controllers/stress.js
@@ -0,0 +1,58 @@
+import Em from 'ember';
+import RemoveButton from '../views/remove-button-cell';
+var set = Em.set;
+var get = Em.get;
+
+var IndexController = Em.Controller.extend({
+	tableColumns: [
+		{
+			name: 'a',
+			label: 'A',
+			order: 1,
+			type: 'number'
+		},
+		{
+			name: 'b',
+			label: 'B',
+			order: 2,
+			type: 'number'
+		},
+		{
+			name: 'c',
+			label: 'C',
+			order: 3,
+			type: 'number'
+		},
+		{
+			name: 'd',
+			label: 'D',
+			order: 4,
+			type: 'number'
+		},
+		{
+			name: 'e',
+			label: 'E',
+			order: 5,
+			type: 'number'
+		}
+	],
+	tableData: [],
+	actions: {
+		add: function (num) {
+			var label = 'Add %@ rows'.fmt(num);
+			console.time(label);
+			for (var i = 0; i < num; i++) {
+				this.get('tableData').pushObject({
+					a: Math.random(),
+					b: Math.random(),
+					c: Math.random(),
+					d: Math.random(),
+					e: Math.random()
+				});
+			}
+			console.timeEnd(label);
+		}
+	}
+});
+
+export default IndexController;
--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -5,7 +5,8 @@ var Router = Ember.Router.extend({
 	location: config.locationType
 });
 
-Router.map(function() {
+Router.map(function () {
+	this.route('stress');
 });
 
 export default Router;
