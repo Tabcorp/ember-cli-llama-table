8f0846e1139ab8cba064a96ad1942bcb969e85fb
j- <jh@live.com.au>
2015-02-02 09:57:30 +1100

    Moved action into generic button cell view

--- tests/dummy/app/views/button-cell.js
+++ tests/dummy/app/views/button-cell.js
@@ -1,7 +1,16 @@
 import LlamaBodyCell from '../views/llama-body-cell';
 
 var ButtonCell = LlamaBodyCell.extend({
-	layoutName: 'button-cell'
+	layoutName: 'button-cell',
+	actions: {
+		click: function () {
+			var controller = this.get('controller');
+			var rows = controller.get('rows');
+			var row = this.get('content');
+			var index = rows.indexOf(row);
+			controller.sendAction(this.get('actionName'), index);
+		}
+	}
 });
 
 export default ButtonCell;
--- tests/dummy/app/views/remove-button-cell.js
+++ tests/dummy/app/views/remove-button-cell.js
@@ -2,15 +2,7 @@ import ButtonCell from './button-cell';
 
 var RemoveButtonCell = ButtonCell.extend({
 	buttonText: 'Remove',
-	actions: {
-		click: function () {
-			var controller = this.get('controller');
-			var rows = controller.get('rows');
-			var row = this.get('content');
-			var index = rows.indexOf(row);
-			controller.sendAction('removeRow', index);
-		}
-	}
+	actionName: 'removeRow'
 });
 
 export default RemoveButtonCell;
