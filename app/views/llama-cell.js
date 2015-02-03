ad61a4248546a66b0c1ab4e2713862c52e7c4339
Bobby Sciacchitano <sciacchitanob@luxbet.com>
2015-02-03 13:23:21 +1100

    Added option for a cell to be clickable, fires action.

--- app/views/llama-cell.js
+++ app/views/llama-cell.js
@@ -1,7 +1,15 @@
 import Em from 'ember';
 
 var LlamaCell = Em.View.extend({
-	classNames: 'llama-cell'
+	classNames: 'llama-cell',
+
+	click: function() {
+		if (this.get('column.isClickable')) {
+			var controller = this.get('controller');
+			controller.sendAction('cellClick', this.get('content'), this.get('column.name'));
+		}
+	}
+
 });
 
 export default LlamaCell;
