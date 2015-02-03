b3c3594705a1e58cad3e11f55a4147d792661db8
j- <jh@live.com.au>
2015-02-03 13:47:54 +1100

    Fixed column definition bindings

--- addon/controllers/columns.js
+++ addon/controllers/columns.js
@@ -3,21 +3,30 @@ import Column from './column';
 var ArrayProxy = Em.ArrayProxy;
 var Sortable = Em.SortableMixin;
 
-var mapColumn = function (column) {
-	return Column.create({
-		content: column
-	});
-};
-
-var Columns = ArrayProxy.extend(Sortable, {
+var ControllerArray = ArrayProxy.extend({
+	itemController: Em.ObjectController,
+	mapController: function (obj) {
+		return this.itemController.create({
+			content: obj
+		});
+	},
 	arrangedContent: function () {
-		return this.get('content').map(mapColumn);
+		return this.get('content').map(this.mapController, this);
 	}.property('content'),
 	contentArrayDidChange: function (arr, i, removedCount, addedCount) {
 		var addedObjects = arr.slice(i, i + addedCount);
-		addedObjects = addedObjects.map(mapColumn);
+		addedObjects = addedObjects.map(this.mapController, this);
 		this.get('arrangedContent').replace(i, removedCount, addedObjects);
 	}
+});
+
+var Columns = ArrayProxy.extend(Sortable, {
+	content: function (key, value) {
+		return ControllerArray.create({
+			itemController: Column,
+			content: value
+		});
+	}.property()
 });
 
 export default Columns;
