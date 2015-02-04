6de20cf360cd215fe6e1bb15d1be18847d4803e1
j- <jh@live.com.au>
2015-02-04 13:06:24 +1100

    Implemented column min and max widths

--- addon/mixins/resize-columns.js
+++ addon/mixins/resize-columns.js
@@ -1,23 +1,35 @@
 import Em from 'ember';
 import ResizeController from 'llama-table/controllers/resize-controller';
 var get = Em.get;
+var set = Em.set;
+var isEmpty = Em.isEmpty;
 
 var ResizeColumns = Em.Mixin.create({
-	resizeColumnRef: null,
-	resizeColumnWidth: null,
+	resizeColumn: null,
+	resizeBeginWidth: null,
 
 	resizeController: ResizeController.create(),
 
 	updateResize: function () {
+		var beginWidth = this.get('resizeBeginWidth');
+		var column = this.get('resizeColumn');
+		var maxWidth = get(column, 'maxWidth');
+		var minWidth = get(column, 'minWidth');
 		var deltaX = this.get('resizeController.deltaX');
-		var newWidth = Math.max(this.get('resizeColumnWidth') + deltaX, 10);
-		this.set('resizeColumnRef.width', newWidth);
+		var newWidth = beginWidth + deltaX;
+		if (!isEmpty(minWidth)) {
+			newWidth = Math.max(newWidth, minWidth);
+		}
+		if (!isEmpty(maxWidth)) {
+			newWidth = Math.min(newWidth, maxWidth);
+		}
+		set(column, 'width', newWidth);
 	}.observes('resizeController.deltaX'),
 
 	actions: {
 		startResize: function (e, column) {
-			this.set('resizeColumnRef', column);
-			this.set('resizeColumnWidth', get(column, 'width'));
+			this.set('resizeColumn', column);
+			this.set('resizeBeginWidth', get(column, 'width'));
 			this.get('resizeController').send('startResize', e);
 		}
 	}
