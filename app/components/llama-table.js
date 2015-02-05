1678d47f79e4075037d43a65933e4acbe920dc82
j- <jh@live.com.au>
2015-02-05 13:33:35 +1100

    Started documenting component

--- app/components/llama-table.js
+++ app/components/llama-table.js
@@ -6,12 +6,44 @@ import Columns from 'llama-table/controllers/columns';
 import Rows from 'llama-table/controllers/rows';
 var get = Em.get;
 
+/**
+ * Llama Table Ember component.
+ * See https://github.com/luxbet/ember-cli-llama-table.
+ * @class LlamaTable
+ * @constructor
+ * @extends Ember.Component
+ * @uses ResizeColumnsMixin
+ */
 var LlamaTable = Em.Component.extend(ResizeColumns, {
 	classNames: 'llama-table-component',
 
-	// column definitions
+	/**
+	 * Column definitions array
+	 * @property {Object[]} columns
+	 * @public
+	 * @see https://github.com/luxbet/ember-cli-llama-table/wiki/Column-definition
+	 */
 	columns: null,
 
+	/**
+	 * Row values array
+	 * @property {Object[]} rows
+	 * @public
+	 */
+	rows: null,
+
+	/**
+	 * Component configuration
+	 * @property {Object} config
+	 * @public
+	 * @see https://github.com/luxbet/ember-cli-llama-table/wiki/Table-configuration
+	 */
+	config: null,
+
+	/**
+	 * Column definitions array with added sorting functionality.
+	 * @property {Ember.ArrayProxy} sortedColumns
+	 */
 	sortedColumns: function () {
 		return Columns.create({
 			sortProperties: ['order'],
@@ -20,9 +52,10 @@ var LlamaTable = Em.Component.extend(ResizeColumns, {
 		});
 	}.property(),
 
-	// table data
-	rows: null,
-
+	/**
+	 * Row values array with added sorting functionality.
+	 * @property {Ember.ArrayProxy} sortedRows
+	 */
 	sortedRows: function () {
 		var options = {
 			sortProperties: this.get('sortProperties'),
@@ -42,28 +75,67 @@ var LlamaTable = Em.Component.extend(ResizeColumns, {
 		return Rows.create(options);
 	}.property(),
 
+	/**
+	 * Enables sorting columns by clicking headers.
+	 * @property {Boolean} isSortable
+	 * @optional
+	 * @default true
+	 */
 	isSortable: function () {
 		return !(this.get('config.isSortable') === false);
 	}.property('config.isSortable'),
+
+	/**
+	 * Enables resizing columns by dragging header boundaries.
+	 * @property {Boolean} isResizable
+	 * @optional
+	 * @default true
+	 */
 	isResizable: function () {
 		return !(this.get('config.isResizable') === false);
 	}.property('config.isResizable'),
 
+	/**
+	 * Column names to sort table by.
+	 * @property {String[]} sortProperties
+	 */
 	sortProperties: Em.computed.alias('config.sortProperties'),
+
+	/**
+	 * Sort columns in ascending order.
+	 * @property {Boolean} sortAscending
+	 * @optional
+	 * @default true
+	 */
 	sortAscending: function () {
 		return !(this.get('config.sortAscending') === false);
 	}.property('config.sortAscending'),
 
+	/**
+	 * Triggers a row sort order update. Observes the `sortAscending` property.
+	 * @method updateRowSortOrder
+	 */
 	updateRowSortOrder: function () {
 		this.set('sortedRows.sortAscending', this.get('sortAscending'));
 	}.observes('sortAscending'),
 
+	/**
+	 * Column definitions grouped into sets.
+	 * @property {Object[][]} columngroups
+	 */
 	columngroups: function () {
 		var columns = this.get('sortedColumns');
 		// single group for now
 		return [columns];
 	}.property('sortedColumns'),
 
+	/**
+	 * Returns the cell at a given row/column position.
+	 * @method findCellAtPosition
+	 * @param {Object} row Reference to row data
+	 * @param {Object} col Reference to column definition
+	 * @return {jQuery} Cell at row/column position
+	 */
 	findCellAtPosition: function (row, col) {
 		var $columns = this.$('.llama-body-column');
 		var $column = $columns.eq(col);
@@ -72,13 +144,22 @@ var LlamaTable = Em.Component.extend(ResizeColumns, {
 		return $cell;
 	},
 
-	// component configuration
-	config: null,
-
+	/**
+	 * Lookup a column type and get the cell constructor.
+	 * @method getCellType
+	 * @param {String} name Column type name
+	 * @return {Function} Cell constructor
+	 */
 	getCellType: function (name) {
 		return this.getConfigCellType(name) || this.getDefaultCellType(name);
 	},
 
+	/**
+	 * Lookup a column type in the table config and get the cell constructor.
+	 * @method getConfigCellType
+	 * @param {String} name Column type name
+	 * @return {Function} Cell constructor
+	 */
 	getConfigCellType: function (name) {
 		var types = this.get('config.types');
 		if (Em.isBlank(types)) return null;
@@ -87,6 +168,12 @@ var LlamaTable = Em.Component.extend(ResizeColumns, {
 		return get(type, 'view');
 	},
 
+	/**
+	 * Lookup a built in column type and get the cell constructor.
+	 * @method getDefaultCellType
+	 * @param {String} name Column type name
+	 * @return {Function} Cell constructor
+	 */
 	getDefaultCellType: function (name) {
 		switch (name) {
 			case 'number':
