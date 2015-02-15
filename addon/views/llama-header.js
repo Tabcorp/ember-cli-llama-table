c8d08d9c9e981695b06df6e4777eec9f6f10a4e8
j- <jh@live.com.au>
2015-02-16 10:21:49 +1100

    Use collection views for header and body

--- addon/views/llama-header.js
+++ addon/views/llama-header.js
@@ -1,8 +1,18 @@
 import Em from 'ember';
+var get = Em.get;
+var set = Em.set;
 
-var LlamaHeader = Em.View.extend({
-	templateName: 'llama-header',
-	classNames: 'llama-header'
+var LlamaHeader = Em.CollectionView.extend({
+	classNames: 'llama-header',
+
+	content: Em.computed.alias('controller.columngroups'),
+	itemViewClass: Em.computed.alias('controller.HeaderColumngroupView'),
+
+	createChildView: function (View, attrs) {
+		var columns = get(attrs, 'content');
+		set(attrs, 'columns', columns);
+		return this._super(View, attrs);
+	}
 });
 
 export default LlamaHeader;
