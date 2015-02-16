07c1e5f92facdef83dcdaaa7654202f0a1dbaea8
j- <jh@live.com.au>
2015-02-16 11:10:56 +1100

    Return default value from computed property if value is blank

--- addon/computed.js
+++ addon/computed.js
@@ -2,6 +2,7 @@ import Em from 'ember';
 var computed = Em.computed;
 var get = Em.get;
 var set = Em.set;
+var isBlank = Em.isBlank;
 
 export var defaultValue = function (watchKey, defaultValue) {
 	return computed(watchKey, function (setKey, value) {
@@ -12,7 +13,7 @@ export var defaultValue = function (watchKey, defaultValue) {
 		}
 		// getter
 		value = get(this, watchKey);
-		return value === undefined ? defaultValue : value;
+		return isBlank(value) ? defaultValue : value;
 	});
 };
 
