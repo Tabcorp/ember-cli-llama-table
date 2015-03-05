71270b46467deee1d26c7d5681540dc5adbb2df0
Bobby Sciacchitano <sciacchitanob@luxbet.com>
2015-03-05 11:07:36 +1100

    Added cursor to clickable cells to identify them as clickable.

--- addon/views/llama-cell.js
+++ addon/views/llama-cell.js
@@ -1,7 +1,8 @@
 import Em from 'ember';
 
 var LlamaCell = Em.View.extend({
-	classNames: 'llama-cell'
+	classNames: 'llama-cell',
+	classNameBindings: ['column.isClickable:cell-clickable'],
 });
 
 export default LlamaCell;
