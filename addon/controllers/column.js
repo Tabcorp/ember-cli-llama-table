5f5bd5052c8d678de0d36ec856e3e88a0551b2e4
j- <jh@live.com.au>
2015-03-06 14:51:55 +1100

    Cells are no longer clickable by default

--- addon/controllers/column.js
+++ addon/controllers/column.js
@@ -5,7 +5,7 @@ var get = Em.get;
 var ColumnController = Em.ObjectProxy.extend({
 	width: defaultValue('content.width', 200),
 	minWidth: defaultValue('content.minWidth', 50),
-	isClickable: defaultValue('content.isClickable', true)
+	isClickable: defaultValue('content.isClickable', false)
 });
 
 export default ColumnController;
