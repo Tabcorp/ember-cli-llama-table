74bef73ac9ea80426ffc1cf50834306d4ef453e4
j- <jh@live.com.au>
2015-03-06 16:42:18 +1100

    Fixed deprecation warning in container view

--- addon/views/llama-table.js
+++ addon/views/llama-table.js
@@ -2,11 +2,21 @@ import Em from 'ember';
 
 var LlamaTable = Em.ContainerView.extend({
 	classNames: 'llama-table',
-	childViews: ['HeaderView', 'BodyView'],
 	HeaderView: Em.computed.alias('controller.HeaderView'),
 	BodyView: Em.computed.alias('controller.BodyView'),
 	headerView: Em.computed.alias('childViews.0'),
-	bodyView: Em.computed.alias('childViews.1')
+	bodyView: Em.computed.alias('childViews.1'),
+	init: function () {
+		this._super();
+		var headerView = this.get('HeaderView').create({
+			container: this.container
+		});
+		this.pushObject(headerView);
+		var bodyView = this.get('BodyView').create({
+			container: this.container
+		});
+		this.pushObject(bodyView);
+	}
 });
 
 export default LlamaTable;
