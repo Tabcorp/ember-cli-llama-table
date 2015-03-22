c03fe797640223296e15169008faa921d4430a1f
j- <jh@live.com.au>
2015-03-23 10:43:32 +1100

    Added config property to cell view

--- addon/views/llama-cell.js
+++ addon/views/llama-cell.js
@@ -1,7 +1,16 @@
 import Em from 'ember';
 
 var LlamaCell = Em.View.extend({
-	classNames: 'llama-cell'
+	classNames: 'llama-cell',
+	config: function () {
+		var types = this.get('controller.config.types');
+		if (!Em.isArray(types)) {
+			return null;
+		}
+		var name = this.get('column.name');
+		var type = types.findBy('name', name);
+		return type;
+	}.property('column.name', 'controller.config.types')
 });
 
 export default LlamaCell;
