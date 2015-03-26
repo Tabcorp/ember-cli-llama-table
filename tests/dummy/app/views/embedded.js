adf6bda727ae6a2bacee45d7ce73d33e01dbad8c
-j <jh@live.com.au>
2015-03-26 11:23:21 +1100

    Added subcontent tables

--- tests/dummy/app/views/embedded.js
+++ tests/dummy/app/views/embedded.js
@@ -2,9 +2,38 @@ import Em from 'ember';
 
 var EmbeddedView = Em.View.extend({
 	templateName: 'embedded',
-	fullName: function () {
-		return this.get('content.given_name') + ' ' + this.get('content.family_name');
-	}.property('content.given_name', 'content.family_name')
+	tableColumns: [
+		{
+			name: 'foo',
+			label: 'Foo',
+			order: 1
+		},
+		{
+			name: 'bar',
+			label: 'Bar',
+			order: 2
+		}
+	],
+	tableData: [
+		{
+			foo: 'abc',
+			bar: 'def'
+		},
+		{
+			foo: 'ghi',
+			bar: 'jkl'
+		}
+	],
+	tableConfig: {
+		maxHeight: 60
+	},
+	setSubcontentHeight: function () {
+		var $el = this.$();
+		if ($el) {
+			var height = $el.outerHeight();
+			this.set('content.subcontentHeight', height + 20);
+		}
+	}.on('didInsertElement')
 });
 
 export default EmbeddedView;
