fa72cf2ede108548f7317a18f2d9fb1191206af2
-j <jh@live.com.au>
2015-04-01 16:19:21 +1100

    Converted row controller to object proxy in order to suppress warnings

--- addon/controllers/row.js
+++ addon/controllers/row.js
@@ -1,7 +1,8 @@
 import Em from 'ember';
 import { defaultValue } from 'llama-table/computed';
 
-var RowController = Em.ObjectController.extend({
+var RowController = Em.ObjectProxy.extend({
+	content: Em.computed.alias('model'),
 	isExpanded: false,
 	height: defaultValue('content.height', 30),
 	subcontentHeight: Em.computed.reads('height')
