5f3e8ef2053ade6fc50c4a1f210f339bf1bf5596
-j <jh@live.com.au>
2015-04-02 10:31:31 +1100

    Inherit R&M episode controller in demo app from row controller

--- tests/dummy/app/controllers/rm-episode.js
+++ tests/dummy/app/controllers/rm-episode.js
@@ -1,6 +1,7 @@
 import Em from 'ember';
+import RowController from 'llama-table/controllers/row';
 
-var EpisodeController = Em.ObjectController.extend({
+var EpisodeController = RowController.extend({
 	description_length: Em.computed.alias('description.length')
 });
 
