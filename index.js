e343728520f89de29dc8a315e0842c0938aa41ca
j- <jh@live.com.au>
2015-04-14 09:44:59 +1000

    Revert "Fixed `number-format` module include"
    This reverts commit f10d375b41a9e58752c1a6fa0f9b0314b4290311.

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 /* jshint node: true */
 'use strict';
 
-var path = require('path');
-
 module.exports = {
-	name: 'llama-table',
-
-	included: function (app, parentAddon) {
-		(app || parentAddon).import('bower_components/number-formatter/lib/format.js');
-	},
-
-	blueprintsPath: function () {
-		return path.join(__dirname, 'blueprints');
-	}
+  name: 'llama-table'
 };
