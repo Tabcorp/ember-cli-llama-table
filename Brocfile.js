754f265f892dc1f6abb9fa0a3129422d06fe52f9
j- <jh@live.com.au>
2015-04-14 16:38:46 +1000

    Reverted all number formatting

--- Brocfile.js
+++ Brocfile.js
@@ -17,6 +17,4 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/number-formatter/lib/format.js');
-
 module.exports = app.toTree();
