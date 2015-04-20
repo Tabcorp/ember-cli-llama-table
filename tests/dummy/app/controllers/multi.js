6d549ae7d3852604565d167f53ec382a487ad0c0
j- <jh@live.com.au>
2015-04-20 13:56:19 +1000

    Make multi-table demo more obvious

--- tests/dummy/app/controllers/multi.js
+++ tests/dummy/app/controllers/multi.js
@@ -3,15 +3,27 @@ var copy = Em.copy;
 
 var cols = [
 	{ name: 'foo', label: 'Foo' },
-	{ name: 'bar', label: 'Bar' }
+	{ name: 'bar', label: 'Bar' },
+	{ name: 'baz', label: 'Baz' },
+	{ name: 'qux', label: 'Qux' }
 ];
 
 var data = [
-	{ foo: 'a1', bar: 'b1' },
-	{ foo: 'a2', bar: 'b2' }
+	{ foo: 'a0', bar: 'b0', baz: 'c0', qux: 'd0' },
+	{ foo: 'a1', bar: 'b1', baz: 'c1', qux: 'd1' },
+	{ foo: 'a2', bar: 'b2', baz: 'c2', qux: 'd3' },
+	{ foo: 'a3', bar: 'b3', baz: 'c3', qux: 'd3' },
+	{ foo: 'a4', bar: 'b4', baz: 'c4', qux: 'd4' },
+	{ foo: 'a5', bar: 'b5', baz: 'c5', qux: 'd5' },
+	{ foo: 'a6', bar: 'b6', baz: 'c6', qux: 'd6' },
+	{ foo: 'a7', bar: 'b7', baz: 'c7', qux: 'd7' },
+	{ foo: 'a8', bar: 'b8', baz: 'c8', qux: 'd8' },
+	{ foo: 'a9', bar: 'b9', baz: 'c9', qux: 'd9' }
 ];
 
-var config = {};
+var config = {
+	maxHeight: 149
+};
 
 var MultiController = Em.Controller.extend({
 	colsA: copy(cols, true),
@@ -22,6 +34,8 @@ var MultiController = Em.Controller.extend({
 	rowsB: copy(data, true),
 	rowsC: data,
 	rowsD: data,
+	configA: copy(config, true),
+	configB: copy(config, true),
 	configC: config,
 	configD: config,
 	tableMove: function (direction, table) {
