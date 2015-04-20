444a40e10dd43b4ffc57a8a2b406b7b0cbd56886
j- <jh@live.com.au>
2015-04-20 10:54:17 +1000

    Removed unused footer cell view

--- addon/views/llama-footer-cell.js
+++
@@ -1,8 +0,0 @@
-import Em from 'ember';
-import LlamaBodyCell from './llama-body-cell';
-
-var LlamaFooterCell = LlamaBodyCell.extend({
-	classNames: ['llama-footer-cell']
-});
-
-export default LlamaFooterCell;
